4519719e20a8165e7aa90adef6b716f29d530831
Aram Baghdasaryan <xquack@gmail.com>
2014-10-26 17:28:07 +0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	    <meta name="Author" content="Aram Baghdasaryan, aram.baghdasaryan@gmail.com">
+
+        <title>Aram Baghdasaryan - Web Design & Development</title>
+
+        <meta name="description" content="Web design and development by Aram Baghdasaryan">
+        <meta name="keywords" content="jiromm.info, jiromm, web, web design, web development, aram baghdasaryan, web applications">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	    <meta property="og:title" content="jiromm - Web design and development by Aram Baghdasaryan"/>
+	    <meta property="og:description" content="Web design and development by Aram Baghdasaryan"/>
+	    <meta property="og:site_name"content="jiromm"/>
+	    <meta property="fb:admins"content="aram.baghdasaryan"/>
+	    <meta property="og:type" content="website"/>
+	    <meta property="og:url" content="http://jiromm.com/"/>
+	    <meta property="og:image" content="http://jiromm.com/img/jr-big.jpg"/>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+	    <div id="fb-root"></div>
+
+	    <script>
+		    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		    ga('create', 'UA-51997825-1', 'jiromm.com');
+		    ga('send', 'pageview');
+	    </script>
+
+        <div class="container">
+
+	        <header>
+		        <a href="http://jiromm.com/" class="jr"></a>
+		        <section class="nina">
+			        <img src="img/me.jpg" alt="Aram Baghdasaryan">
+		        </section>
+	        </header>
+
+            <section class="row">
+                <div class="span6 offset3">
+                    <h2 class="author center">Aram Baghdasaryan</h2>
+<!--                    <p class="center silver">Nina Manukyan in photo, because she's more beautiful than me.</p>-->
+                    <ul class="social clearfix">
+	                    <li class="facebook"><a href="http://facebook.com/aram.baghdasaryan"></a></li>
+	                    <li class="twitter"><a href="http://twitter.com/jiromm"></a></li>
+	                    <li class="linkedin"><a href="http://www.linkedin.com/in/arambaghdasaryan/"></a></li>
+	                    <li class="gplus"><a href="https://plus.google.com/u/0/105565152422871843409"></a></li>
+	                    <li class="youtube"><a href="http://www.youtube.com/scragaram"></a></li>
+	                    <li class="soundcloud"><a href="https://soundcloud.com/jiromm"></a></li>
+	                    <li class="skype"><a href="skype:q3.double"></a></li>
+	                    <li class="email"><a href="mailto:aram.baghdasaryan@gmail.com"></a></li>
+                    </ul>
+                </div>
+            </section>
+
+            <footer>
+                <p>&copy; 2013 jiromm. I like to think &amp; believe that all rights reserved.</p>
+	            <p>Web design &amp; development is my job.</p>
+	            <p class="fb-like">
+		            <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fjiromm.info&amp;send=false&amp;layout=button_count&amp;width=50&amp;show_faces=false&amp;font=tahoma&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=137156559808291" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:80px; height:21px; vertical-align: middle" allowTransparency="true"></iframe>
+	            </p>
+            </footer>
+
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
